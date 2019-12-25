0917d869a7e378d08f3333db7a36dbbc3168aa2a
ing <inyoman_gurnitha@yahoo.com>
2019-12-26 02:07:47 +0700

    Practicing urls from core/urls

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse('Home page')
+
+def about(request):
+	return HttpResponse('About page')
+
+def contact(request):
+	return HttpResponse('Contact page')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about),
+    path('contact/', contact),
 ]
