eaa5b748b8c75771a85d18edf535b14ff5ad763b
ing <inyoman_gurnitha@yahoo.com>
2019-12-26 10:00:35 +0700

    5-Created model for Customer, Product and Order

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# from .models import Customer
+# from .models import Product
+# from .models import Order
+from .models import * # equal to line 5 to 7
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
